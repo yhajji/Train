7d425177e732792bbeaa9375a2efff5e7b6e2d83
Samir OUZZINEN <ouzz.samir@gmail.com>
2016-02-19 10:25:11 +0000

    initialize train project

---
+++ src/main/java/sii/maroc/Train.java
@@ -0,0 +1,29 @@
+package sii.maroc;
+
+public class Train {
+
+	public Train(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void detachEnd() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void detachHead() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean fill() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object print() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
